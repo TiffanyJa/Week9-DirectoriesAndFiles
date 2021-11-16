3f658bd733160e280d9aad89a9d07dbbc0fd6994
Tiffany Jaakmees <tjaakmee@tktk.ee>
2021-11-16 21:44:39 +0200

    add new projects

---
+++ MyShoppingListFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyShoppingListFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\User\sample\ShoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Add an item to your shopping list:");
+                    string useritem = Console.ReadLine();
+                    myShoppingList.Add(useritem);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+            }
+            Console.Clear();
+            foreach (string item in myShoppingList)
+            {
+                Console.WriteLine($"Your item: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\User\sample";
+            string newDirectory = @"shoppingList";
+
+            string fileName = "myShoppingList.txt";
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+
+        }
+    }
+}
